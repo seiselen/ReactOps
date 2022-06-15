aa4b6df3b95b2e6096db576250fb04d6209cff32
Steven Eiselen <seiselen24@email.arizona.edu>
2022-06-14 20:41:37 -0700

    minor additions
    comment header box in main, pulled react DOM lib ref for now, etc.

---
+++ Project 01/main.js
@@ -0,0 +1,22 @@
+/*######################################################################
+|>>> Project : 'Opening Act' (React.js 'Learn-To-Command' Ops)
++-----------------------------------------------------------------------
+| Description: Several sentence blurb
+| Author:      Steven Eiselen, <affiliations as/applicable>
+| Resource 01: 'Beginner's Tutorial for React JavaScript Library [2022]'
+| Res 01 Link: https://www.youtube.com/watch?v=bMknfKXIFA8
+| Libraries:   React (16.7.0 - development) | [https://reactjs.org/]
++-----------------------------------------------------------------------
+| Implementation Notes:
+|  > Discoveries / problems / To-Do's / etc.
++-----------------------------------------------------------------------
+|            < OIC/OEC : Praise Be The Honourable Mentors! >            
++#####################################################################*/
+
+
+
+
+const element = React.createElement('div', {
+  className: 'container',
+  children: Date.now().toLocaleString()
+});
