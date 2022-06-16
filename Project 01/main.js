3c25ae438829a1d5eef47268049a4c7ec82ecc5c
Steven Eiselen <seiselen24@email.arizona.edu>
2022-06-16 08:49:04 -0700

    Update main.js

--- Project 01/main.js
+++ Project 01/main.js
@@ -1,24 +1,6 @@
 /*######################################################################
-|>>> Project : 'Opening Act' (React.js 'Learn-To-Command' Ops)
-+-----------------------------------------------------------------------
-| Description: Several sentence blurb
-| Author:      Steven Eiselen, <affiliations as/applicable>
-| Resource 01: 'Beginner's Tutorial for React JavaScript Library [2022]'
-| Res 01 Link: https://www.youtube.com/watch?v=bMknfKXIFA8
-| Libraries:   React (16.7.0 - development) | [https://reactjs.org/]
-+-----------------------------------------------------------------------
-| Implementation Notes:
-|  > Blank CodePen: https://codepen.io/pen
-+-----------------------------------------------------------------------
-| Questions:
-|  > Are we using JSX or no?
-|  > Common/Suggested means of creating [local] server to run webpages
-|    as to please the 'CORS Gods'?
-+-----------------------------------------------------------------------
-|            < OIC/OEC : Praise Be The Honourable Mentors! >            
+|>>> Project : 'Opening Act' (React.js 'Learn-To-Command' Ops)       
 +#####################################################################*/
-
-
 
 
 const element = React.createElement('div', {
